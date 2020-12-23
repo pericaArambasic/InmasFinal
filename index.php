5435af9ae1c356b10f634da450a5848db4e6f1cc
Perica <=>
2020-12-23 10:25:06 +0100

    added sort by price

--- index.php
+++ index.php
@@ -9,9 +9,19 @@ include 'functions.php';
 $search = '';
 $pdo = pdo_connect_mysql();
 $search = $_POST['inventory_name'];
-$stmt = $pdo->prepare('SELECT i . id_inventory, inventory_name, inventory_price, c . category_name  
+if (!isset($_POST['sort'])) {
+    $stmt = $pdo->prepare('SELECT i . id_inventory, inventory_name, inventory_price, c . category_name  
                                 FROM inventory i, categories c  
                                 WHERE i . inventory_name LIKE ? AND  i.id_cat = c.id_cat');
+} elseif ($_POST['sort'] == 'asc') {
+    $stmt = $pdo->prepare('SELECT i . id_inventory, inventory_name, inventory_price, c . category_name  
+                                FROM inventory i, categories c  
+                                WHERE i . inventory_name LIKE ? AND  i.id_cat = c.id_cat ORDER BY inventory_price ASC');
+} elseif ($_POST['sort'] == 'desc') {
+    $stmt = $pdo->prepare('SELECT i . id_inventory, inventory_name, inventory_price, c . category_name  
+                                FROM inventory i, categories c  
+                                WHERE i . inventory_name LIKE ? AND  i.id_cat = c.id_cat ORDER BY inventory_price DESC ');
+}
 $stmt->execute(array("%$search%"));
 $search_result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -29,11 +39,10 @@ $search_result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </div>
     <form action="index.php" method="post">
         <h4>Price:</h4>
-        <input type="submit" value="sort">
+        <input type="submit" class="btn btn-info" style="color: white" value="sort">
         <select name="sort" id="sort">
-            <option selected value="">-</option>
             <option value="asc">Ascending</option>
-            <option value="dsc">Descending</option>
+            <option value="desc">Descending</option>
         </select>
         <br>
     </form>
